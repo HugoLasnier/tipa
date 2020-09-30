39630755383907690aaeeec0e5cf4ed791859e82
HugoLasnier <hugo@lasnier.org>
2020-09-30 15:17:28 +0200

    add differents data types of the json

--- examples/json/json.cpp
+++ examples/json/json.cpp
@@ -18,17 +18,25 @@ using namespace tipa;
 
 int main(int argc, char *argv[])
 {
+
+	// token
     const token tk_char = create_lib_token("^([0-9a-zA-Z]{1})");
-    rule r_char = rule(tk_char);
-    rule r_string = rule("\"") >> *r_char >> rule("\"");
-    rule key_value = r_string >> rule(":") >> r_string;
+    const token tk_digit = create_lib_token("^([0-9]{1})");
 
+    //data types
+    rule r_anyType;
+    rule r_number = *rule(tk_digit);
+    rule r_string = rule("\"") >> *rule(tk_char) >> rule("\"");
+    rule r_boolean = rule ("true") | rule("false");
+    rule r_null = rule("null");
+
+	rule r_array = rule("[")>> *(r_anyType >> rule(",")) >> r_anyType >>rule("]");
+	rule key_value = r_string >> rule(":") >> r_anyType;
     rule key_values = *(key_value >> rule(",")) >> key_value;
-    rule bracket = (rule("{") >> key_values >> rule("}")) | rule("{") >> rule("}");
+    rule r_object= (rule("{") >> key_values >> rule("}")) | rule("{") >> rule("}");
 
-    rule root = *(bracket >> rule(",")) >> bracket;
-
-
+    r_anyType = (r_object | r_array | r_null | r_boolean | r_string | r_number);
+    rule root = r_object;
 
     
     //----------------  end of parser specification -------------
@@ -36,15 +44,14 @@ int main(int argc, char *argv[])
     // An example of json file to parse
     std::stringstream sst(
     	"{"
-         "\" key1 \" : \" val1 \","
-        "\" key2 \" : \" val2 \","
-        "\" key3 \" : \" val3 \""
-        "},"
-        "{"
-        "\" key1 \" : \" val1 \""
-        "},"
-        "{}"
-        
+    	"\"aString\" : \"toto\","
+    	"\"null\" : null,"
+    	"\"Array\" : [1 , [1,2,3]],"
+    	"\"aBool\" : true,"
+    	"\"aBool2\" : false,"
+    	"\"aNumber\" : 1234,"
+    	"\"anObject\" : {\"aa\" : \"b\",\"b\" :  {}}"
+    	"}"
     );
     
     parser_context pc;
