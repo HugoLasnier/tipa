87f693b1dbd566354f51f58abd5880094b6ca66d
HugoLasnier <hugo@lasnier.org>
2020-11-22 18:27:41 +0100

    wip, creation de la structure de donnée contenant le json parsé

--- examples/json/json.cpp
+++ examples/json/json.cpp
@@ -5,9 +5,11 @@
 #include <fstream>
 #include <memory>
 #include <tuple>
+#include <map>
+
 
 #include <tinyparser.hpp>
-
+using namespace std;
 using namespace tipa;
 
 
@@ -16,33 +18,147 @@ using namespace tipa;
     todo
 */
 
-int main(int argc, char *argv[])
-{
+
+class Value {
+    public:
+        virtual string toString(){return "value";};
+        virtual ~Value() {}
+};
+
+class IntValue : public Value {
+private:
+    int n;
+public: 
+    virtual string toString(){ return to_string(this->n);}
+    IntValue(int n){ this->n = n; }
+};
+
+class BoolValue : public Value {
+private:
+    bool b;
+public: 
+    virtual string toString(){ return this->b ? "true" : "false";}
+    BoolValue(bool b){ this->b = b; }
+};
+
+class FloatValue : public Value {
+private:
+    float f;
+public: 
+    virtual string toString(){ return to_string(this->f);}
+    FloatValue(float f){this->f = f;}
+};
+
+class StringValue : public Value {
+private:
+    string s;
+public:
+    virtual string toString(){ return this->s;}
+    StringValue(string s){this->s = s;}
+};
+
+class NullValue : public Value {
+public:
+    virtual string toString(){ return "null";}
+};
+
+class builder {
+    stack<string> st1;
+    stack< shared_ptr<Value> > st2;
+    map<string, shared_ptr<Value>> js;
+
+public:
+
+    void make_Key(parser_context &pc){
+        auto x = pc.collect_tokens();
+        if (x.size() < 1) throw string("Error in collecting variable");
+        string v = x[x.size() - 1].second;
+        st1.push(v);
+    }
+
+    void make_KeyValue(parser_context &pc){
+        string key = st1.top(); st1.pop();
+        auto val = st2.top(); st2.pop();
+        js[key] = val;
+    }
+    
+    void make_StringValue(parser_context &pc){
+        auto x = pc.collect_tokens();
+        if (x.size() < 1) throw string("Error in collecting variable");
+        string v = x[x.size() - 1].second;
+        auto sv = make_shared<StringValue>(v);
+        st2.push(sv);
+    }
+
+    void make_NullValue(parser_context &pc){
+    	auto x = pc.collect_tokens();
+        if (x.size() < 1) throw string("Error in collecting variable");
+        string v = x[x.size() - 1].second;
+        auto nv = make_shared<NullValue>();
+        st2.push(nv);
+    }
+
+    void make_IntValue(parser_context &pc){
+        auto x = pc.collect_tokens();
+        if (x.size() < 1) throw string("Error in collecting variable");
+        int v = atoi(x[x.size()-1].second.c_str());
+        auto iv = make_shared<IntValue>(v);
+        st2.push(iv);
+    }
+
+    void make_FloatValue(parser_context &pc){
+        auto x = pc.collect_tokens();
+        if (x.size() < 1) throw string("Error in collecting variable");
+        float v = stof(x[x.size()-1].second.c_str());
+        auto fv = make_shared<FloatValue>(v);
+        st2.push(fv);
+    }
+
+    void make_BoolValue(parser_context &pc){
+        auto x = pc.collect_tokens();
+        if (x.size() < 1) throw string("Error in collecting variable");
+        bool b = "true" == x[x.size() - 1].second;
+        auto bv = make_shared<BoolValue>(b);
+        st2.push(bv);
+    }
+
+    void printContent(){
+        map<string, shared_ptr<Value>>::iterator it;
+
+        for( it = js.begin(); it != js.end(); it++ ){
+            cout << it->first << ":" << it-> second->toString() << endl;
+        }
+    }
+};
+
+int main(int argc, char *argv[]){
 
 	// token
-    const token tk_char = create_lib_token("^([0-9a-zA-Z]{1})");
-    const token tk_digit = create_lib_token("^([0-9]{1})");
+    const token tk_null = create_lib_token("^null");
+  	const token tk_true = create_lib_token("^true");
+    const token tk_false = create_lib_token("^false");
+    const token tk_float = create_lib_token("[+-]?([0-9]*[.])[0-9]+");
 
     //data types
+
     rule r_anyType;
-    rule r_number = *rule(tk_digit);
-    rule r_string = rule("\"") >> *rule(tk_char) >> rule("\"");
-    rule r_boolean = rule ("true") | rule("false");
-    rule r_null = rule("null");
-
-	rule r_array = rule("[")>> *(r_anyType >> rule(",")) >> r_anyType >>rule("]");
-	rule key_value = r_string >> rule(":") >> r_anyType;
-    rule key_values = *(key_value >> rule(",")) >> key_value;
-    rule r_object= (rule("{") >> key_values >> rule("}")) | rule("{") >> rule("}");
-
-    r_anyType = (r_object | r_array | r_null | r_boolean | r_string | r_number);
-    rule root = r_object;
-
+    rule r_number = rule(tk_int);
+    rule r_float = rule(tk_float);
+    rule r_boolean = rule (tk_true) | rule(tk_false); // | rule("false");
+    rule r_null = rule(tk_null); //rule r_null = rule("null");
+    rule r_string = rule("\"") >> rule(tk_ident) >> rule("\"");
+    rule r_key = rule("\"") >> rule(tk_ident) >> rule("\"") >> rule(":");
+	rule r_array = rule(tk_op_sq)>> *(r_anyType >> rule(tk_comma)) >> r_anyType >>rule(tk_cl_sq);
+	rule key_value = r_key >> r_anyType;
+    rule key_values = *(key_value >> rule(tk_comma)) >> key_value;
+    rule r_object= (rule(tk_op_br) >> key_values >> rule(tk_cl_br)) | rule(tk_op_br) >> rule(tk_cl_br);
+    r_anyType = ( r_object | r_array | r_null | r_boolean | r_string | r_float | r_number );
+    rule root =  r_object;
     
     //----------------  end of parser specification -------------
 
     // An example of json file to parse
-    std::stringstream sst(
+    /*stringstream sst(
     	"{"
     	"\"aString\" : \"toto\","
     	"\"null\" : null,"
@@ -52,14 +168,39 @@ int main(int argc, char *argv[])
     	"\"aNumber\" : 1234,"
     	"\"anObject\" : {\"aa\" : \"b\",\"b\" :  {}}"
     	"}"
+    );*/
+
+    stringstream sst(
+        "{"
+        "\"aString\" : \"Hello\","
+        "\"a1\" : 1,"
+        "\"b2\" : 25,"
+        "\"c3\" : 3,"
+        "\"booleanKey\" : true,"
+        "\"bkey2\" : false,"
+        "\"nullKey\" : null,"
+        "\"Pi\" : 3.14"
+        "}"
     );
+
+    builder b; 
+    using namespace std::placeholders;
+    r_null.   set_action(std::bind(&builder::make_NullValue,            &b, _1));
+   	r_boolean.   set_action(bind(&builder::make_BoolValue,            &b, _1));
+    r_float.   set_action(std::bind(&builder::make_FloatValue,            &b, _1));
+    r_number.   set_action(std::bind(&builder::make_IntValue,            &b, _1));
+    r_key.   set_action(bind(&builder::make_Key,            &b, _1));
+    r_string.   set_action(bind(&builder::make_StringValue,            &b, _1));
+    key_value.   set_action(bind(&builder::make_KeyValue,            &b, _1));
     
     parser_context pc;
     pc.set_stream(sst);
 
     bool f = parse_all(root, pc);
-    std::cout << "Parser status : " << std::boolalpha << f << std::endl;
+    b.printContent();
+    cout << "Parser status : " << boolalpha << f << endl;
     if (!f) {
-        std::cout << pc.get_formatted_err_msg() << std::endl;
+        cout << pc.get_formatted_err_msg() << endl;
     }
 }
+
